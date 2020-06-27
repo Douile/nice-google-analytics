897ab466a4a5344c954845159115913528217431
Douile <25043847+Douile@users.noreply.github.com>
2020-06-27 23:54:54 +0100

    Fix integrity

--- generatecss.sh
+++ generatecss.sh
@@ -2,11 +2,17 @@
 
 # Create base64 and integrity hash for css
 
-B64=$(cleancss ./src/nicega.css | openssl base64 -A)
-INTEGRITY=$(echo $B64 | openssl dgst -sha512 -binary | openssl base64 -A)
+css=$(cleancss -format "breaks:afterAtRule=on;space:beforeValue" ./src/nicega.css)
+enc=$(echo -n "$css" | openssl base64 -A)
+sha256=$(echo -n "$css" | openssl dgst -sha256 -binary | openssl base64 -A)
+sha512=$(echo -n "$css" | openssl dgst -sha512 -binary | openssl base64 -A)
 
-echo "Base64"
-echo "$B64"
+echo "Raw CSS"
+echo "$css"
+
+echo "Base64 CSS"
+echo "$enc"
 
 echo "Integrity"
-echo "sha512-$INTEGRITY"
+echo "sha256-$sha256"
+echo "sha512-$sha512"
